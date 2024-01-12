1eed7a748ffb38a143f7a3584f10222dada7866b
Alex Yip <alex.yip@softwire.com>
2024-01-12 12:43:24 +0000

    Add add items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -11,3 +11,10 @@ app.config.from_object(Config())
 def index():
     items = get_items()
     return render_template('index.html', items=items)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form.get('title')
+    add_item(title)
+    return redirect('/')
