a0e084af621bdbe574dd34f636e0d4c003276d43
Alex Yip <alex.yip@softwire.com>
2024-01-26 12:24:52 +0000

    Add get_board using trello api

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, render_template, request
 
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import add_item
+from todo_app.data.trello_items import get_board
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -9,7 +10,7 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = get_items()
+    items = get_board()
     return render_template('index.html', items=items)
 
 
---
+++ todo_app/data/trello_items.py
@@ -0,0 +1,45 @@
+import os
+
+import requests
+
+TRELLO_API_HOST = 'https://api.trello.com/1'
+TRELLO_API_KEY = os.getenv('TRELLO_API_KEY')
+TRELLO_API_TOKEN = os.environ.get('TRELLO_API_TOKEN')
+TRELLO_BOARD_ID = os.environ.get('TRELLO_BOARD_ID')
+
+TRELLO_TODO_LIST_ID = os.environ.get('TRELLO_TODO_LIST_ID')
+TRELLO_DONE_LIST_ID = os.environ.get('TRELLO_DONE_LIST_ID')
+
+
+def get_board():
+    url = "{url}/boards/{id}/lists".format(
+        url=TRELLO_API_HOST, id=TRELLO_BOARD_ID
+    )
+
+    headers = {"Accept": "application/json"}
+
+    query = {'key': TRELLO_API_KEY, 'token': TRELLO_API_TOKEN, 'cards': 'open', 'fields': 'id,name,cards',
+             'card_fields': 'id,name'}
+
+    response = requests.request(
+        "GET",
+        url,
+        headers=headers,
+        params=query
+    )
+
+    cards = []
+    for trello_list in response.json():
+        for card in trello_list['cards']:
+            if trello_list['id'] == TRELLO_DONE_LIST_ID:
+                status = 'Done'
+            else:
+                status = 'To do'
+
+            cards.append({
+                'id': card['id'],
+                'title': card['name'],
+                'status': status
+            })
+
+    return cards
